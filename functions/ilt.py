Corrected l-curve calculation and plotting

functions/ilt.py
@@ -43,8 +43,9 @@ def Contin(t, F, bound, Nz, alpha):
     [2] Lawson, C., & Hanson, R. (1974), Solving Least Squares Problems, SIAM
     """
 
-    F = F - F[-1]
+    F = F - np.average(F[-2:-1])
     F = np.abs(F)
+    F = F/max(F)
 
     # pre-processing
     #if len(t) != len(F):

functions/residuals.py
@@ -32,6 +32,10 @@ def residuals(s, C, ay, Methods, Reg_L1, Reg_L2, Reg_C, Bounds, Nz):
 
     data = []
 
+    C = C - np.average(C[-2:-1])
+    C = np.abs(C)
+    C = C/max(C)
+
     for i in Methods:
 
         if len(Methods) > 1:
@@ -46,7 +50,7 @@ def residuals(s, C, ay, Methods, Reg_L1, Reg_L2, Reg_C, Bounds, Nz):
                 data = laplace(s, C - C[-1], Nz, Reg_L1, v, Reg_C, Bounds, Methods)
                 e, f, C_restored = data[0][0], data[0][1], data[0][2]
 
-                res.append(np.linalg.norm(np.abs(C - C[-1]) - np.abs(C_restored), ord = 2)**2)
+                res.append(np.linalg.norm(np.abs(C) - np.abs(C_restored), ord = 2)**2)
                 sol.append(np.linalg.norm(f, ord = 2)**2)
             alpha = alpha_L2
             break
@@ -59,7 +63,7 @@ def residuals(s, C, ay, Methods, Reg_L1, Reg_L2, Reg_C, Bounds, Nz):
                 data = laplace(s, C - C[-1], Nz, Reg_L1, Reg_L2, v, Bounds, Methods)
                 e, f, C_restored = data[0][0], data[0][1], data[0][2]
 
-                res.append(np.linalg.norm(np.abs(C - C[-1]) - np.abs(C_restored), ord = 2)**2)
+                res.append(np.linalg.norm(np.abs(C) - np.abs(C_restored), ord = 2)**2)
                 sol.append(np.linalg.norm(f, ord = 2)**2)
             alpha = alpha_C
             break